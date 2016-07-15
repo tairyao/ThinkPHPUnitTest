84eef09d3782ac5e0b25df2b76f74a77163012c4
tairyao <420993860@qq.com>
2016-07-15 19:16:15 +0800

    单元测试例子

---
+++ Application/Home/Conf/config.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+
+    //数据库配置
+    'testDB' => array(
+        'db_type'  	=> 'mysql',
+        'db_user'  	=> 'root',
+        'db_pwd'   	=> 'root',
+        'db_host'  	=> '127.0.0.1',
+        'db_port'  	=> '3306',
+        'db_name'  	=> 'test',
+        'db_charset'=> 'utf8',
+    ),
+);
